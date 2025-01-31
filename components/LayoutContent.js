3c80c56f140f15090321e097f2cd125798f78f05
Kushagra Bhardwaj <kush2012bhardwaj@gmail.com>
2025-01-31 18:52:06 +0530

    Update LayoutContent.js

--- components/LayoutContent.js
+++ components/LayoutContent.js
@@ -7,7 +7,7 @@ import Footer from "@/components/footer"
 import WhatsappFloat from "@/components/whatsapp-float"
 
 export default function LayoutContent({ children }) {
-  const pathname = usePathname()
+  // const pathname = usePathname()
 
   useEffect(() => {
     window.scrollTo(0, 0)
