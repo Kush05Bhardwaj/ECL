d6120dbd24d854c60efb1629969efbae698c1750
Kushagra Bhardwaj <kush2012bhardwaj@gmail.com>
2025-01-31 18:56:27 +0530

    Update LayoutContent.js

--- components/LayoutContent.js
+++ components/LayoutContent.js
@@ -1,14 +1,11 @@
 "use client"
 
 import { useEffect } from "react"
-import { usePathname } from "next/navigation"
 import Header from "@/components/header"
 import Footer from "@/components/footer"
 import WhatsappFloat from "@/components/whatsapp-float"
 
 export default function LayoutContent({ children }) {
-  const pathname = usePathname()
-
   useEffect(() => {
     window.scrollTo(0, 0)
   }, [])
